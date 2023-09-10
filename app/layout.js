93b171cf11b33c2c8502e5d31bdab59f46f7f85e
Samy AMINE <samyaminework@gmail.com>
2023-09-11 00:50:27 +0200

    Navbar and started main page

---
+++ app/layout.js
@@ -0,0 +1,27 @@
+import '@styles/globals.css';
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+
+export const metadata = {
+    title: "AI Prompt",
+    description: "Discover & share AI prompts",
+}
+
+function RootLayout({ children }) {
+    return (
+        <html lang='en'>
+            <body>
+                <div className='main'>
+                    <div className='gradient' />
+                </div>
+
+                <main className='app'>
+                    <Nav />
+                    {children}
+                </main>
+            </body>
+        </html>
+    );
+}
+
+export default RootLayout;
